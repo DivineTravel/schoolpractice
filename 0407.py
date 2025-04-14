23f9eac47972243850616d3156b591bcb3ebe921
DivineTravel <99454832+DivineTravel@users.noreply.github.com>
2025-04-14 02:28:23 -0700

    Update 0407.py

--- 0407.py
+++ 0407.py
@@ -5,17 +5,12 @@ print(a[3][1])
 print(a[3][2][1])
 
 name = input("Enter your name: ")
-time.sleep(0.1)
 age = int(input("Enter your age: "))
-time.sleep(0.1)
 price = float(input("Enter the price: "))
-time.sleep(0.1)
-print("Please wait...")
-time.sleep(1)
 print("Hello", name)
 print("You are", age, "years old.")
 print("The price is", price)
-time.sleep(1)
+
 n1 = 2
 n2 = 5
 sum = n1 + n2
